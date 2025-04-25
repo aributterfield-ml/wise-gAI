0e43c508d96dec77f0bfabb79ccc92f1cc6c1108
Ari Butterfield <aributterfield@proton.me>
2025-04-24 23:56:14 -0500

    configured API skeleton

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from proverbs.views import get_proverbs
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('proverbs/', get_proverbs),
 ]
--- backend/proverbs/views.py
+++ backend/proverbs/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+@api_view(['GET'])
+def get_proverbs(request):
+    query = request.GET.get('query', '')
+    
+    # Dummy response for now
+    results = [
+        f"Proverb matching '{query}' 1",
+        f"Proverb matching '{query}' 2",
+        f"Proverb matching '{query}' 3",
+    ]
+    return Response(results)
